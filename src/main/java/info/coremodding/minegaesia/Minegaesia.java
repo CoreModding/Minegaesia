5869b1ac1ee039a93303a89e9446ebef9241c71a
MushroomLT <kasparas6@hotmail.com>
2014-06-12 16:16:54 +0300

    Directory tree

---
+++ src/main/java/info/coremodding/minegaesia/Minegaesia.java
@@ -0,0 +1,8 @@
+package info.coremodding.minegaesia;
+
+import cpw.mods.fml.common.Mod;
+
+@Mod(modid = "core_minegaesia", name = "The Minegaesia Mod", version = "0.1-PREALPHA")
+public class Minegaesia {
+	
+}
